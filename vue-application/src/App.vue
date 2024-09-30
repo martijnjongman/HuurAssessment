c031a87d833e044f9b94b5ecf0029a9f8908dde5
Martijn Jongman <martijnjongman9@gmail.com>
2024-09-30 14:03:37 +0200

    Setup project

---
+++ vue-application/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="max-w-screen-2xl m-auto py-14">
+    <div class="flex justify-between text-brand-dark-gray pb-8 border-b border-brand-light-gray">
+      <h1 class="text-2xl lg:text-4xl font-semibold">Huurwoningen in Nederlands</h1>
+      
+      <span class="text-base lg:text-xl font-semibold">30 resultaten</span>
+    </div>
+  </div>
+</template>
---
+++ vue-application/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'brand-dark-gray': '#111827',
+        'brand-light-gray': '#e5e7eb',
+      },
+      fontFamily: {
+        brandFont: ['Urbanist', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
